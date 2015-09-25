521d716e403c850f56d094533ab8a1215f4b1b74
Rodrigo Ruz <rodrigo.ruz.v@gmail.com>
2015-09-25 16:05:42 -0300

    Added code for "Getting the getter and setter of a property using RTTI" article

---
+++ RTTI/Getting the getter and setter of a property using RTTI/Sample.dpr
@@ -0,0 +1,154 @@
+
+program Sample;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.RTTI,
+  System.TypInfo,
+  System.SysUtils,
+  System.Classes,
+  uRttiHelper in 'uRttiHelper.pas';
+
+type
+  //The getters and setters methods of the property must emit RTTI info this implies which depending of the visibility of these methods
+  //you will need to instruct to the compiler generate RTTI info adding this sentence to your class
+  {$RTTI EXPLICIT METHODS([vcPrivate])}
+  TBar = class
+  private
+    FReadOnlyProp: string;
+    FWriteOnlyProp: string;
+    function GetReadBaseProp: string; virtual;
+    procedure SetWriteBaseProp(const Value: string); virtual;
+  public
+    property ReadBaseProp: string read GetReadBaseProp;
+    property WriteBaseProp: string write SetWriteBaseProp;
+  end;
+
+  //The getters and setters methods of the property must emit RTTI info this implies which depending of the visibility of these methods
+  //you will need to instruct to the compiler generate RTTI info adding this sentence to your class
+  {$RTTI EXPLICIT METHODS([vcPrivate])}
+  TFoo = class(TBar)
+  private
+    function GetReadOnlyPropwGet: string;
+    procedure SetWriteOnlyPropwSet(const Value: string);
+    function GetReadBaseProp: string; override;
+  public
+    property ReadOnlyProp: string read FReadOnlyProp;
+    property WriteOnlyProp: string Write FWriteOnlyProp;
+    property ReadOnlyPropwGet: string read GetReadOnlyPropwGet;
+    property WriteOnlyPropwSet: string write SetWriteOnlyPropwSet;
+  end;
+
+  { TBar }
+
+function TBar.GetReadBaseProp: string;
+begin
+
+end;
+
+procedure TBar.SetWriteBaseProp(const Value: string);
+begin
+
+end;
+
+{ TFoo }
+
+
+function TFoo.GetReadBaseProp: string;
+begin
+
+end;
+
+function TFoo.GetReadOnlyPropwGet: string;
+begin
+
+end;
+
+
+
+procedure TFoo.SetWriteOnlyPropwSet(const Value: string);
+begin
+
+end;
+
+procedure DumpPropInfo(AClass: TObject);
+var
+  LContext: TRttiContext;
+  LType: TRttiType;
+  LProp: TRttiProperty;
+  LPropInfo: PPropInfo;
+begin
+  //Get the typeinfo of the class
+  LType := LContext.GetType(AClass.ClassInfo);
+
+  for LProp in LType.GetProperties() do
+    if LProp is TRttiInstanceProperty then
+    begin
+      //Get the pointer to the PPropInfo
+      LPropInfo := TRttiInstanceProperty(LProp).PropInfo;
+      Writeln(Format('%-18s GetProc %p SetProc %p', [LProp.Name, LPropInfo.GetProc, LPropInfo.SetProc]));
+    end;
+end;
+
+
+//This method uses the TRttiProperty Helper  to resolve the names of the getters and setters
+procedure DumpPropInfoExt(AClass: TObject);
+var
+  LContext: TRttiContext;
+  LType: TRttiType;
+  LProp: TRttiProperty;
+  LPropInfo: PPropInfo;
+
+  LField: TRttiField;
+  LMethod: TRttiMethod;
+begin
+  LType := LContext.GetType(AClass.ClassInfo);
+  for LProp in LType.GetProperties() do
+    if LProp is TRttiInstanceProperty then
+    begin
+      LPropInfo := TRttiInstanceProperty(LProp).PropInfo;
+      Writeln(Format('%-18s GetProc %p SetProc %p', [LProp.Name, LPropInfo.GetProc, LPropInfo.SetProc]));
+
+      if LProp.IsReadable then
+      begin
+        LField := LProp.GetterField;
+        if LField <> nil then
+          Writeln(Format('  Getter Field Name %s', [LField.Name]))
+        else
+        begin
+          LMethod := LProp.GetterMethod(AClass);
+          if LMethod <> nil then
+            Writeln(Format('  Getter Method Name %s', [LMethod.Name]))
+        end;
+      end;
+
+      if LProp.IsWritable then
+      begin
+        LField := LProp.SetterField;
+        if LField <> nil then
+          Writeln(Format('  Setter Field Name %s', [LField.Name]))
+        else
+        begin
+          LMethod := LProp.SetterMethod(AClass);
+          if LMethod <> nil then
+            Writeln(Format('  Setter Method Name %s', [LMethod.Name]))
+        end;
+      end;
+
+    end;
+end;
+
+begin
+  try
+    DumpPropInfo(TFoo.Create);
+    Writeln;
+    DumpPropInfoExt(TFoo.Create);
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  Readln;
+
+end.
---
+++ RTTI/Getting the getter and setter of a property using RTTI/uRttiHelper.pas
@@ -0,0 +1,180 @@
+unit uRttiHelper;
+
+interface
+
+uses
+  Rtti,
+  TypInfo;
+
+  type
+     TRttiPropertyHelper = class helper for TRttiProperty
+  private
+    function GetSetterField  : TRttiField;
+    function GetGetterField  : TRttiField;
+  public
+    property SetterField : TRttiField read GetSetterField;
+    property GetterField : TRttiField read GetGetterField;
+    function SetterMethod (Instance : TObject) : TRttiMethod;
+    function GetterMethod (Instance : TObject) : TRttiMethod;
+  end;
+
+
+implementation
+
+const
+{$IF SizeOf(Pointer) = 4}
+  PROPSLOT_MASK_F    = $000000FF;
+{$ELSEIF SizeOf(Pointer) = 8}
+  PROPSLOT_MASK_F    = $00000000000000FF;
+{$ENDIF}
+
+
+function IsField(P: Pointer): Boolean; inline;
+begin
+  Result := (IntPtr(P) and PROPSLOT_MASK) = PROPSLOT_FIELD;
+end;
+
+function GetCodePointer(Instance: TObject; P: Pointer): Pointer; inline;
+begin
+  if (IntPtr(P) and PROPSLOT_MASK) = PROPSLOT_VIRTUAL then // Virtual Method
+    Result := PPointer(PNativeUInt(Instance)^ + (UIntPtr(P) and $FFFF))^
+  else // Static method
+    Result := P;
+end;
+
+
+function GetPropGetterMethod(Instance: TObject; AProp : TRttiProperty) : TRttiMethod;
+var
+  LPropInfo : PPropInfo;
+  LMethod: TRttiMethod;
+  LCodeAddress : Pointer;
+  LType : TRttiType;
+  LocalContext: TRttiContext;
+begin
+  Result:=nil;
+  if (AProp.IsReadable) and (AProp.ClassNameIs('TRttiInstancePropertyEx')) then
+  begin
+    //get the PPropInfo pointer
+    LPropInfo:=TRttiInstanceProperty(AProp).PropInfo;
+    if (LPropInfo<>nil) and (LPropInfo.GetProc<>nil) and not IsField(LPropInfo.GetProc) then
+    begin
+      //get the real address of the ,ethod
+      LCodeAddress := GetCodePointer(Instance, LPropInfo^.GetProc);
+      //get the Typeinfo for the current instance
+      LType:= LocalContext.GetType(Instance.ClassType);
+      //iterate over the methods of the instance
+      for LMethod in LType.GetMethods do
+      begin
+         //compare the address of the currrent method against the address of the getter
+         if LMethod.CodeAddress=LCodeAddress then
+           Exit(LMethod);
+      end;
+    end;
+  end;
+end;
+
+function GetPropSetterMethod(Instance: TObject; AProp : TRttiProperty) : TRttiMethod;
+var
+  LPropInfo : PPropInfo;
+  LMethod: TRttiMethod;
+  LCodeAddress : Pointer;
+  LType : TRttiType;
+  LocalContext: TRttiContext;
+begin
+  Result:=nil;
+  if (AProp.IsWritable) and (AProp.ClassNameIs('TRttiInstancePropertyEx')) then
+  begin
+    //get the PPropInfo pointer
+    LPropInfo:=TRttiInstanceProperty(AProp).PropInfo;
+    if (LPropInfo<>nil) and (LPropInfo.SetProc<>nil) and not IsField(LPropInfo.SetProc) then
+    begin
+      LCodeAddress := GetCodePointer(Instance, LPropInfo^.SetProc);
+      //get the Typeinfo for the current instance
+      LType:= LocalContext.GetType(Instance.ClassType);
+      //iterate over the methods
+      for LMethod in LType.GetMethods do
+      begin
+         //compare the address of the currrent method against the address of the setter
+         if LMethod.CodeAddress=LCodeAddress then
+           Exit(LMethod);
+      end;
+    end;
+  end;
+end;
+
+function GetPropGetterField(AProp : TRttiProperty) : TRttiField;
+var
+  LPropInfo : PPropInfo;
+  LField: TRttiField;
+  LOffset : Integer;
+begin
+  Result:=nil;
+  //Is a readable property?
+  if (AProp.IsReadable) and (AProp.ClassNameIs('TRttiInstancePropertyEx')) then
+  begin
+    //get the propinfo of the porperty
+    LPropInfo:=TRttiInstanceProperty(AProp).PropInfo;
+    //check if the GetProc represent a field
+    if (LPropInfo<>nil) and (LPropInfo.GetProc<>nil) and IsField(LPropInfo.GetProc) then
+    begin
+      //get the offset of the field
+      LOffset:= IntPtr(LPropInfo.GetProc) and PROPSLOT_MASK_F;
+      //iterate over the fields of the class
+      for LField in AProp.Parent.GetFields do
+         //compare the offset the current field with the offset of the getter
+         if LField.Offset=LOffset then
+           Exit(LField);
+    end;
+  end;
+end;
+
+function GetPropSetterField(AProp : TRttiProperty) : TRttiField;
+var
+  LPropInfo : PPropInfo;
+  LField: TRttiField;
+  LOffset : Integer;
+begin
+  Result:=nil;
+  //Is a writable property?
+  if (AProp.IsWritable) and (AProp.ClassNameIs('TRttiInstancePropertyEx')) then
+  begin
+    //get the propinfo of the porperty
+    LPropInfo:=TRttiInstanceProperty(AProp).PropInfo;
+    //check if the GetProc represent a field
+    if (LPropInfo<>nil) and (LPropInfo.SetProc<>nil) and IsField(LPropInfo.SetProc) then
+    begin
+      //get the offset of the field
+      LOffset:= IntPtr(LPropInfo.SetProc) and PROPSLOT_MASK_F;
+      //iterate over the fields of the class
+      for LField in AProp.Parent.GetFields do
+         //compare the offset the current field with the offset of the setter
+         if LField.Offset=LOffset then
+           Exit(LField);
+    end;
+  end;
+end;
+
+
+{ TRttiPropertyHelper }
+
+function TRttiPropertyHelper.GetGetterField: TRttiField;
+begin
+ Result:= GetPropGetterField(Self);
+end;
+
+function TRttiPropertyHelper.GetSetterField: TRttiField;
+begin
+ Result:= GetPropSetterField(Self);
+end;
+
+function TRttiPropertyHelper.GetterMethod(Instance: TObject): TRttiMethod;
+begin
+ Result:= GetPropGetterMethod(Instance, Self);
+end;
+
+function TRttiPropertyHelper.SetterMethod(Instance: TObject): TRttiMethod;
+begin
+ Result:= GetPropSetterMethod(Instance, Self);
+end;
+
+end.
